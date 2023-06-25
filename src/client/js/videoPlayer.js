8f13ece0ef0dcb854b1c88effea0d4a8b0de7ab2
bansCode <banskim0918@gmail.com>
2023-06-25 21:03:41 +0700

    In progress of video player

---
+++ src/client/js/videoPlayer.js
@@ -0,0 +1,59 @@
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volumeRange = document.getElementById("volume");
+
+let volumeValue = 0.5;
+video.volume = volumeValue;
+
+const handlePlayClick = (e) => {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+  playBtn.innerText = video.paused ? "Play" : "Pause";
+};
+
+const handleMuteClick = (event) => {
+  // video should be muted at range 0 and when 'unmute' it should be back to 0.5
+  if (video.muted) {
+    video.muted = false;
+  } else {
+    video.muted = true;
+  }
+  muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+
+  // Below code is same as: volumeRange.value = video.muted ? 0 : volumeValue === "0" ? 0.5 : volumeValue;
+  if (video.muted) {
+    volumeRange.value = 0;
+  } else if (volumeValue === "0") {
+    volumeRange.value = 0.5;
+    video.volume = 0.5;
+  } else {
+    volumeRange.value = volumeValue;
+  }
+};
+
+const handleVolumeInput = (event) => {
+  const {
+    target: { value },
+  } = event;
+
+  if (video.muted) {
+    video.muted = false;
+    muteBtn.innerText = "Mute";
+  }
+  volumeValue = value;
+  video.volume = value;
+
+  if (Number(volumeValue) === 0) {
+    muteBtn.innerText = "Unmute";
+    video.muted = true;
+  }
+};
+
+playBtn.addEventListener("click", handlePlayClick);
+muteBtn.addEventListener("click", handleMuteClick);
+volumeRange.addEventListener("input", handleVolumeInput);
